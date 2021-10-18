4c5d1e6af7d2b458a4184547d7df33ffef00c7b6
Leonid Shagabutdinov <leonid@shagabutdinov.com>
2021-10-18 19:32:37 +0300

    [master] implement search request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        'https://ac.cnstrc.com/search/apple?key=key_fygjntHGW7usvxC8',
+      );
+
+      const data = await response.json()
+      setData(data);
+    })()
+  }, [])
+
+  if (!data) {
+    return 'loading...';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <h1>Results for: apples</h1>
+
+      <div className="container">
+        {data.response.results.map((item) => (
+          <div className="item">
+            <img className="image" src={item.data.image_url} />
+
+            <div className="name">
+              {item.value}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
